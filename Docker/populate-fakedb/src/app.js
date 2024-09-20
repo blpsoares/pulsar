600dfb8c926237040c39958d6bddc54a7b74d57e
Bryan Soares <bryan.luccas@eletromidia.com.br>
2024-09-20 19:19:55 -0300

    feat: create a simple image and script to up fake-db

---
+++ Docker/populate-fakedb/src/app.js
@@ -0,0 +1,28 @@
+import { conn } from './db/conn.js';
+import { populateDB } from './services/populate.js';
+
+const simpleCollections = [
+  'simpleCollection1',
+  'simpleCollection2',
+  'simpleCollection3',
+  'simpleCollection4',
+];
+const complexCollections = ['complexCollection1', 'complexCollection2'];
+
+const main = async () => {
+  const client = await conn();
+  const db = client.db('aurora');
+
+  const options = {
+    simpleCollections: simpleCollections,
+    complexCollections: complexCollections,
+    collectionSize: 5e4,
+    batchSize: 1000,
+    db,
+  };
+
+  await populateDB(options);
+  client.close();
+};
+
+await main();
