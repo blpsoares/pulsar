9acea27c69454839ee07e57ca99d346c3ac32010
Bryan Soares <bryan.luccas@eletromidia.com.br>
2024-09-24 16:22:20 -0300

    feat: create custom type for dumo options

---
+++ src/types/parse-yml.d.ts
@@ -0,0 +1,16 @@
+type DumpYmlOptions = {
+  command: {
+    dump: {
+      source: {
+        uri: string;
+        db: string;
+      };
+      destination: {
+        uri: string;
+        db: string;
+      };
+      collections: string[];
+      output: string;
+    };
+  };
+};
