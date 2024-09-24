f367923280fc8a68a8c675a52d110d86efd0b38c
Bryan Soares <bryan.luccas@eletromidia.com.br>
2024-09-24 16:17:51 -0300

    feat: move errors classes to new folder

---
+++ src/classes/errors-class.ts
@@ -0,0 +1,26 @@
+import chalk from 'chalk';
+import { customLog } from '../utils/responses.chalk';
+
+export class YmlToJsonError extends Error {
+  code?: string;
+
+  constructor(message: string, code?: string) {
+    super(message);
+    this.name = 'YmlToJsonError';
+    this.code = code;
+  }
+}
+
+export class CustomError extends Error {
+  breadcrumb: string;
+
+  constructor(message: string, breadcrumb: string) {
+    super(message);
+    this.breadcrumb = breadcrumb;
+  }
+
+  logError() {
+    customLog('error', `${this.message}`);
+    return chalk.hex('#ff7c00').bold(this.breadcrumb);
+  }
+}
