af664685c2a69a8109d879d6bab1541698b1c7c7
Bryan Soares <bryan.luccas@eletromidia.com.br>
2024-09-24 16:23:28 -0300

    feat: remove mongo client

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,13 @@
-import { conn } from './db/conn';
 import { showTitle } from './utils/show-cli-title';
+import { Command } from 'commander';
+import dumpDbFn from './cli/dump';
 
 await showTitle();
-const client = await conn();
+
+const program = new Command();
+
+program.version('1.0.0').description('Mongo pulsar cli to sync data');
+
+program.command('dump <file>').action(dumpDbFn);
+
+program.parse(process.argv);
