62cc3c3df73bfde4dc789e318402b98f44271e19
Bryan Soares <bryan.luccas@eletromidia.com.br>
2024-09-25 19:27:27 -0300

    chore(title): config breaklines after of title

--- src/utils/show-cli-title.ts
+++ src/utils/show-cli-title.ts
@@ -3,11 +3,11 @@ import chalk from 'chalk';
 
 export const showTitle = () => {
   return new Promise((resolve) => {
-    figlet('PULSAR', { font: 'Doom', verticalLayout: 'controlled smushing' }, (err, title) => {
+    figlet('_| PULSAR |_', { font: 'Big', verticalLayout: 'controlled smushing' }, (err, title) => {
       if (err) {
         console.log('«« PULSAR »»');
       } else {
-        console.log(chalk.hex('#9b00ff').bold(title));
+        console.log(chalk.hex('#9b00ff').bold(title).concat(`\n\n`));
       }
       resolve(true);
     });
