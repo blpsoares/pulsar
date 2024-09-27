9a7732598d25b51092030642e54d108782f0d148
Bryan Soares <bryan.luccas@eletromidia.com.br>
2024-09-27 16:57:16 -0300

    feat: add new level and configure stdout colors

--- src/utils/custom-log.ts
+++ src/utils/custom-log.ts
@@ -1,47 +1,58 @@
 import chalk from 'chalk';
-import { createLogger, format, transports, addColors } from 'winston';
-const { combine, timestamp, printf, colorize } = format;
+import { createLogger, format, transports } from 'winston';
+import { format as formatDate } from 'date-fns';
+import { ptBR } from 'date-fns/locale';
+const { combine, timestamp, printf } = format;
 
-const customLevels = {
-  levels: {
-    error: 0,
-    warn: 1,
-    info: 2,
-    success: 3,
-  },
-  colors: {
-    error: 'red',
-    warn: 'yellow',
-    info: 'blue',
-    success: 'green',
-  },
-};
 const optionsLogs = {
   success: chalk.greenBright,
   error: chalk.redBright,
   info: chalk.gray,
   warn: chalk.yellowBright,
+  debug: chalk.blueBright,
+  levels: {
+    error: 0,
+    info: 1,
+    success: 2,
+    warn: 3,
+    debug: 4,
+  },
 };
-
-addColors(customLevels.colors);
 
 const logFormat = printf(({ level, message, timestamp }) => {
   return `${timestamp} [${level}]: ${message}`;
 });
 
+const getFullDate = (): string => {
+  return formatDate(new Date(), 'dd/MM/yyyy - HH:mm:ss', { locale: ptBR });
+};
+
 const logger = createLogger({
-  levels: customLevels.levels,
-  level: 'success',
-  format: combine(timestamp({ format: 'DD-MM HH:mm:ss' }), logFormat),
+  levels: optionsLogs.levels,
+  level: 'debug',
+  format: combine(
+    timestamp({
+      format: getFullDate,
+    }),
+    logFormat,
+  ),
   transports: [
+    new transports.File({ filename: './src/logs/debug.log', level: 'debug' }),
     new transports.File({ filename: './src/logs/error.log', level: 'error' }),
     new transports.File({ filename: './src/logs/combined.log' }),
   ],
 });
 
 export const customLog = (type: OptionsCustomLogs, message: string) => {
-  console.log(optionsLogs[type].bold(message));
+  const prefix = `[ ${type.toUpperCase()} ]: `;
+  console.log(optionsLogs[type].bold(prefix + message));
   logger.log({ level: type, message });
 };
 
 export default logger;
+
+customLog('error', 'teste de error');
+customLog('warn', 'teste de warn');
+customLog('info', 'teste de info');
+customLog('success', 'teste de success');
+customLog('debug', 'teste de debug');
