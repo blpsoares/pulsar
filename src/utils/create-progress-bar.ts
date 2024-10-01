2259b9e71f3ec94847c5a3aed1861c2cbb1baf6d
Bryan Soares <bryan.luccas@eletromidia.com.br>
2024-10-01 17:54:03 -0300

    style: add new style on progress bar

--- src/utils/create-progress-bar.ts
+++ src/utils/create-progress-bar.ts
@@ -2,9 +2,10 @@ import cliprogress from 'cli-progress';
 
 export const createSingleBar = (start: number, progressMessage: string = 'Progress') => {
   const singleBar = new cliprogress.SingleBar({
-    format: `| {bar} | {percentage}% | {duration_formatted} | {value}/{total} | ${progressMessage} `,
+    format: `⟬{bar}⟭ {percentage}% | {duration_formatted} | {value}/{total} | ${progressMessage}`,
     barCompleteChar: '※',
     barIncompleteChar: '⁍',
+    emptyOnZero: true,
   });
 
   singleBar.start(start, 0);
