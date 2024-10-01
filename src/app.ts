5e5cbf4932c654a23d196de0a9de4ed0f2c3146f
Bryan Soares <bryan.luccas@eletromidia.com.br>
2024-10-01 17:56:18 -0300

    feat: remove docker-compose option to the cli

--- src/app.ts
+++ src/app.ts
@@ -1,31 +1,20 @@
 #!/usr/bin/env bun
 
-import { showTitle } from "./utils/show-cli-title";
-import { Command } from "commander";
-import dumpDbFn from "./cli/dump";
+import { showTitle } from './utils/show-cli-title';
+import { Command } from 'commander';
+import dumpDbFn from './cli/dump';
 
 await showTitle();
 
 const program = new Command();
 
-program.version("1.0.0").description("Mongo pulsar cli to sync data");
-
-// TODO: config pulsar cli to runs on docker
-program.command("on <boolean>").action((on: string) => {
-  const state = on === "true";
-
-  if (!state) {
-    Bun.spawn(["docker-compose", "down"]);
-  } else {
-    Bun.spawn(["docker-compose", "up", "--build", "-d"]);
-  }
-});
+program.version('1.0.0').description('Mongo pulsar cli to sync data');
 
 program
-  .command("dump <file>")
+  .command('dump <file>')
   .option(
-    "-p --parallel <number>",
-    "send a number to export collections in parallel, example: -p 2 or --parallel 2.\nBy default this value is 3."
+    '-p --parallel <number>',
+    'send a number to export collections in parallel, example: -p 2 or --parallel 2.\nBy default this value is 3.',
   )
   .action(dumpDbFn);
 
