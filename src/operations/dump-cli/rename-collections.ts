245b5f3ff08762f97b016c1cf1c3280530a8fe20
Bryan Soares <bryan.luccas@eletromidia.com.br>
2024-10-02 20:47:54 -0300

    feat: add logger to registry estimated names to rename

--- src/operations/dump-cli/rename-collections.ts
+++ src/operations/dump-cli/rename-collections.ts
@@ -1,6 +1,11 @@
 import Bottleneck from 'bottleneck';
 import { logger, customLog } from '../../utils/custom-log';
-import { MongoClient } from 'mongodb';
+import { Db, MongoClient } from 'mongodb';
+
+const dbRenameCollection = async (db: Db, collection: string) => {
+  logger.info(`Init step of rename collections - from: _dump_${collection} | to: ${collection}\n`);
+  await db.renameCollection(`_dump_${collection}`, collection);
+};
 
 export const renameNewCollections = async (
   client: MongoClient,
@@ -10,9 +15,10 @@ export const renameNewCollections = async (
   const db = client.db(dbName);
   const limiter = new Bottleneck({ maxConcurrent: 2 });
   customLog('info', 'Rename all new collections...');
-  const promises = collections.map((col) =>
-    limiter.schedule(() => db.renameCollection(`_dump_${col}`, col)),
+  const promises = collections.map((collection) =>
+    limiter.schedule(() => dbRenameCollection(db, collection)),
   );
+
   try {
     await Promise.all(promises);
   } catch (error) {
