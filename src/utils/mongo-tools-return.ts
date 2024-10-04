aa58c049293040c303894c70a4ec8bc22baed10b
Bryan Soares <bryan.luccas@eletromidia.com.br>
2024-10-03 21:53:06 -0300

    feat: change type and function name

--- src/utils/mongo-tools-return.ts
+++ src/utils/mongo-tools-return.ts
@@ -1,13 +1,13 @@
 // TODO:
 //? Bring messages from collections that were not dumped/restored to this file (making the dump and restore functions cleaner)
-export const mongoToolsReturns = (
-  collectionsStats: MongoToolsReturn[],
+export const MongoStatusReturns = (
+  collectionsStats: MongoStatusReturn[],
   message?: string,
 ): string[][] => {
   const successfulExports: string[] = [];
   const failedExports: string[] = [];
   collectionsStats.forEach((item) => {
-    if (item.sucess) successfulExports.push(item.sucess);
+    if (item.success) successfulExports.push(item.success);
     if (item.failed) failedExports.push(item.failed);
   });
 
