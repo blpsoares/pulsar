42a392615b8722124b156691c84d1ad42492c86f
Bryan Soares <bryanluccas@hotmail.com>
2025-08-07 16:45:24 -0300

    style: :lipstick: change title font

--- src/utils/show-cli-title.ts
+++ src/utils/show-cli-title.ts
@@ -3,11 +3,11 @@ import chalk from 'chalk';
 
 export const showTitle = () => {
   return new Promise((resolve) => {
-    figlet('_| PULSAR |_', { font: 'Big', verticalLayout: 'controlled smushing' }, (err, title) => {
+    figlet('P U L S A R', { font: 'Rectangles', verticalLayout: 'controlled smushing' }, (err, title) => {
       if (err) {
         console.log('«« PULSAR »»');
       } else {
-        console.log(chalk.hex('#9b00ff').bold(title).concat(`\n\n`));
+        console.log(chalk.italic.hex('#9b00ff').bold(title).concat(`\n\n`));
       }
       resolve(true);
     });
