510a0342b246e7ec99bf38a5def18a5ce67b5766
Bryan Soares <bryanluccas@hotmail.com>
2025-08-08 12:51:46 -0300

    feat: :construction: initial watch structure

--- src/cli/watch.ts
+++ src/cli/watch.ts
@@ -1,18 +1,36 @@
-import { Db } from "mongodb";
 import { conn } from "../db/conn";
-import path from "path";
-import { existsSync, mkdirSync } from "fs";
 import parseYml from "../utils/parseYml";
+import { errorHandler } from "../errors/errorHandler";
+import type { Db } from "mongodb";
 
-export async function watchCollections(ymlpath: string){
-  const outputExport = path.resolve(__dirname, '..', '..', 'temp-dump');
-
-  if (!existsSync(outputExport)) mkdirSync(outputExport);
-
-  const options = parseYml<>(ymlpath);
-  const client =  await conn('mongodb+srv://u1-prod-escrita:REDACTED@elmd-aurora-01.9txz8.mongodb.net');
-  const db = client.db('aurora');
-  const collections = (await db.listCollections().toArray()).map((collection) => collection.name);
+export async function getCollections(
+  db: Db,
+  cliParams: WatchOptionsCli,
+  options: WatchYmlOptions,
+  ymlpath: string
+) {
+  let collections: string[] = [];
+    if(cliParams.all){
+      collections = (await db.listCollections().toArray()).map((collection) => collection.name);
+    } else if (options.command.watch.collections){
+      collections = options.command.watch.collections;
+    } else {
+      throw errorHandler(new Error(`No collections to watch on file: ${ymlpath}`))
+    }
+    return collections;
 }
 
-// watchCollections();
+export async function watchCollections(ymlpath: string, cliParams: WatchOptionsCli){
+  const options = parseYml<WatchYmlOptions>(ymlpath);
+  const client =  await conn(options.command.watch.source.uri, 'source');
+  const db = client.db('aurora');
+  try {
+    const collections = await getCollections(db, cliParams, options, ymlpath);
+    console.log(collections);
+  } catch (error) {
+
+  } finally {
+    client.close();
+  }
+}
+
