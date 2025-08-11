9dbea994f55524d9aa3a8b22e7901bf6ea660ee5
Bryan Soares <bryan.luccas@eletromidia.com.br>
2025-08-11 18:46:06 -0300

    style: lint adjusts

--- src/types/parseYml.ts
+++ src/types/parseYml.ts
@@ -1,36 +1,36 @@
-import { z } from 'zod';
+import { z } from "zod";
 
 export const dumpYmlSchema = z.object({
-  command: z.object({
-    dump: z.object({
-      source: z.object({
-        uri: z.string(),
-        db: z.string(),
-      }),
-      destination: z.object({
-        uri: z.string(),
-        db: z.string(),
-      }),
-      collections: z.array(z.string()),
-      queryString: z.string().optional(),
-    }),
-  }),
+	command: z.object({
+		dump: z.object({
+			source: z.object({
+				uri: z.string(),
+				db: z.string(),
+			}),
+			destination: z.object({
+				uri: z.string(),
+				db: z.string(),
+			}),
+			collections: z.array(z.string()).optional(),
+			queryString: z.string().optional(),
+		}),
+	}),
 });
 
 export const watchYmlSchema = z.object({
-  command: z.object({
-    watch: z.object({
-      source: z.object({
-        uri: z.string(),
-        db: z.string(),
-      }),
-      destination: z.object({
-        uri: z.string(),
-        db: z.string(),
-      }),
-      collections: z.array(z.string()).optional(),
-    }),
-  }),
+	command: z.object({
+		watch: z.object({
+			source: z.object({
+				uri: z.string(),
+				db: z.string(),
+			}),
+			destination: z.object({
+				uri: z.string(),
+				db: z.string(),
+			}),
+			collections: z.array(z.string()).optional(),
+		}),
+	}),
 });
 
 export type DumpYmlOptions = z.infer<typeof dumpYmlSchema>;
--- src/utils/parseYml.ts
+++ src/utils/parseYml.ts
@@ -1,30 +1,29 @@
-import fs from 'fs';
-import yaml from 'js-yaml';
-import type { ZodType } from 'zod';
-import { ZodError } from 'zod';
-import { errorHandler } from '../errors/errorHandler';
-import { customLog } from './customLog';
+import fs from "fs";
+import yaml from "js-yaml";
+import type { ZodType } from "zod";
+import { ZodError } from "zod";
+import { errorHandler } from "../errors/errorHandler";
 
 const parseYml = <T>(ymlPath: string, schema: ZodType<T>): T => {
-  if (!fs.existsSync(ymlPath)) {
-    throw errorHandler(new Error(`File not found on path: ${ymlPath}`));
-  }
+	if (!fs.existsSync(ymlPath)) {
+		throw errorHandler(new Error(`File not found on path: ${ymlPath}`));
+	}
 
-  const yml = fs.readFileSync(ymlPath, 'utf-8');
-  const rawData = yaml.load(yml);
+	const yml = fs.readFileSync(ymlPath, "utf-8");
+	const rawData = yaml.load(yml);
 
-  if (!rawData) {
-    throw errorHandler(new Error('YML file is empty or malformed'));
-  }
+	if (!rawData) {
+		throw errorHandler(new Error("YML file is empty or malformed"));
+	}
 
-  try {
-    return schema.parse(rawData);
-  } catch (err) {
-    if (err instanceof ZodError) {
-      throw errorHandler(err, "PARSE:YML:ZOD")
-    }
-    throw err;
-  }
+	try {
+		return schema.parse(rawData);
+	} catch (err) {
+		if (err instanceof ZodError) {
+			throw errorHandler(err, "PARSE:YML:ZOD");
+		}
+		throw err;
+	}
 };
 
 export default parseYml;
