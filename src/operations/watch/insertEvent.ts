16542c26ed78220a6d679548880c1ffa0a35df38
vinimostaco <vini.mostaco@hotmail.com>
2025-08-11 13:54:10 -0300

    feat(watch): :sparkles: add properties hot, ts and hash

--- src/operations/watch/insertEvent.ts
+++ src/operations/watch/insertEvent.ts
@@ -1,14 +1,25 @@
 import type { Document, Collection } from "mongodb";
 import { customLog } from "../../utils/customLog";
+import { Hash } from "../../classes/hashClass";
 
-export async function insertEvent(collection: Collection, doc: Document) {
+export async function insertEvent(
+	collection: Collection,
+	doc: Document,
+	hash: number,
+) {
 	if (!doc) {
 		customLog(
 			"warn",
-			`[${collection}] fullDocument não encontrado. Ignorando.`,
+			`[${collection.namespace}] fullDocument não encontrado. Ignorando.`,
 		);
 		return;
 	}
-	await collection.insertOne(doc);
-	console.log(`[${collection}] Documento enviado para destino`);
+
+	await collection.insertOne({
+		...doc,
+		hot: true,
+		ts: Date.now(),
+		hash: hash,
+	});
+	console.log(`[${collection.namespace}] Documento enviado para destino`);
 }
