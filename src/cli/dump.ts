cb95dc2795b9a35e589e573383d803d3a22d669d
Bryan Soares <bryan.luccas@eletromidia.com.br>
2025-08-11 13:25:31 -0300

    style: biome lint

--- src/cli/dump.ts
+++ src/cli/dump.ts
@@ -1,123 +1,136 @@
-import fs from 'fs';
-import path from 'path';
-import { conn } from '../db/conn';
-import Bottleneck from 'bottleneck';
-import parseYml from '../utils/parseYml';
-import { initDump } from '../operations/dumpCli/dump';
-import { deleteTempFolder } from '../utils/deleteTempFolder';
-import { initRestore } from '../operations/dumpCli/restoreDump';
-import { initRegistrationSync } from '../operations/dumpCli/initSync';
-import { dropOldCollections } from '../operations/dumpCli/dropOldCollections';
-import { renameNewCollections } from '../operations/dumpCli/renameCollections';
-import { customLog } from '../utils/customLog';
-import { dumpYmlSchema, type DumpYmlOptions } from '../types/parseYml';
+import fs from "fs";
+import path from "path";
+import { conn } from "../db/conn";
+import Bottleneck from "bottleneck";
+import parseYml from "../utils/parseYml";
+import { initDump } from "../operations/dump/dump";
+import { deleteTempFolder } from "../utils/deleteTempFolder";
+import { initRestore } from "../operations/dump/restoreDump";
+import { initRegistrationSync } from "../operations/dump/initSync";
+import { dropOldCollections } from "../operations/dump/dropOldCollections";
+import { renameNewCollections } from "../operations/dump/renameCollections";
+import { customLog } from "../utils/customLog";
+import { dumpYmlSchema, type DumpYmlOptions } from "../types/parseYml";
 
-const migrateCollections = async (ymlpath: string, cliParams: DumpOptionsCli) => {
-  const outputExport = path.resolve(__dirname, '..', '..', 'temp-dump');
+const migrateCollections = async (
+	ymlpath: string,
+	cliParams: DumpOptionsCli,
+) => {
+	const outputExport = path.resolve(__dirname, "..", "..", "temp-dump");
 
-  if (!fs.existsSync(outputExport)) fs.mkdirSync(outputExport);
+	if (!fs.existsSync(outputExport)) fs.mkdirSync(outputExport);
 
-  const options = parseYml<DumpYmlOptions>(ymlpath, dumpYmlSchema);
-  const { dump } = options.command;
-  const limiter = new Bottleneck({ maxConcurrent: cliParams.parallel ?? 2 });
+	const options = parseYml<DumpYmlOptions>(ymlpath, dumpYmlSchema);
+	const { dump } = options.command;
+	const limiter = new Bottleneck({ maxConcurrent: cliParams.parallel ?? 2 });
 
-  /**
-   *
-   * ? DUMP COLLECTIONS
-   */
-  const [successExports, failedExports] = await initDump(
-  dump.source,
-  outputExport,
-  limiter,
-  dump.collections,
-  dump.queryString,
-  cliParams.maxRetries
-  );
+	/**
+	 *
+	 * ? DUMP COLLECTIONS
+	 */
+	const [successExports, failedExports] = await initDump(
+		dump.source,
+		outputExport,
+		limiter,
+		dump.collections,
+		dump.queryString,
+		cliParams.maxRetries,
+	);
 
-  if (failedExports.length > 0) {
-    customLog('error', `Failed to restore collections: ${failedExports}`);
-    return;
-  };
+	if (failedExports.length > 0) {
+		customLog("error", `Failed to restore collections: ${failedExports}`);
+		return;
+	}
 
-  /**
-   *
-   * ? RESTORE COLLECTIONS
-   */
-    const [successRestores, failedRestores] = await initRestore(
-      options,
-      successExports,
-      limiter,
-      cliParams.maxRetries
-    );
+	/**
+	 *
+	 * ? RESTORE COLLECTIONS
+	 */
+	const [successRestores, failedRestores] = await initRestore(
+		options,
+		successExports,
+		limiter,
+		cliParams.maxRetries,
+	);
 
-  if (failedRestores.length > 0) {
-    customLog('error', `Failed to restore collections: ${failedRestores}`);
-      return;
-  };
+	if (failedRestores.length > 0) {
+		customLog("error", `Failed to restore collections: ${failedRestores}`);
+		return;
+	}
 
-  /**
-   *
-   * ? CONNECT TO DESTINATION
-   */
-  const client = await conn(dump.destination.uri, 'destination');
+	/**
+	 *
+	 * ? CONNECT TO DESTINATION
+	 */
+	const client = await conn(dump.destination.uri, "destination");
 
-  /**
-   *
-   * ? SET STATE ON __SYNC__ COLLECTION
-   */
-  const [successColds, failedColds] = await initRegistrationSync(
-    options,
-    successRestores,
-    client,
-    limiter,
-  );
+	/**
+	 *
+	 * ? SET STATE ON __SYNC__ COLLECTION
+	 */
+	const [successColds, failedColds] = await initRegistrationSync(
+		options,
+		successRestores,
+		client,
+		limiter,
+	);
 
-  if (failedColds.length > 0) {
-    customLog('info', 'Retrying set cold stats on failed collections');
+	if (failedColds.length > 0) {
+		customLog("info", "Retrying set cold stats on failed collections");
 
-    let [newSuccessColds] = await initRegistrationSync(options, failedColds, client, limiter);
-    successColds.push(...newSuccessColds);
-  }
+		let [newSuccessColds] = await initRegistrationSync(
+			options,
+			failedColds,
+			client,
+			limiter,
+		);
+		successColds.push(...newSuccessColds);
+	}
 
-  /**
-   *
-   * ? DROP ON DATABASE ALL COLLECTIONS DUMPED
-   */
-  const [successDrops, failedDrops] = await dropOldCollections(
-    client,
-    dump.destination.db,
-    successColds,
-    limiter,
-  );
-  if (failedDrops.length > 0) {
-    customLog('info', 'Retrying drop failed collections');
+	/**
+	 *
+	 * ? DROP ON DATABASE ALL COLLECTIONS DUMPED
+	 */
+	const [successDrops, failedDrops] = await dropOldCollections(
+		client,
+		dump.destination.db,
+		successColds,
+		limiter,
+	);
+	if (failedDrops.length > 0) {
+		customLog("info", "Retrying drop failed collections");
 
-    const [newSuccessDrops] = await dropOldCollections(
-      client,
-      dump.destination.db,
-      failedDrops,
-      limiter,
-    );
-    successDrops.push(...newSuccessDrops);
-  }
+		const [newSuccessDrops] = await dropOldCollections(
+			client,
+			dump.destination.db,
+			failedDrops,
+			limiter,
+		);
+		successDrops.push(...newSuccessDrops);
+	}
 
-  /**
-   *
-   * ? REMOVE ON DESTINATION DATABASE ALL _dump_ PREFIX ON RESTORED COLLECTIONS
-   */
-  await renameNewCollections(client, dump.destination.db, successDrops, limiter);
+	/**
+	 *
+	 * ? REMOVE ON DESTINATION DATABASE ALL _dump_ PREFIX ON RESTORED COLLECTIONS
+	 */
+	await renameNewCollections(
+		client,
+		dump.destination.db,
+		successDrops,
+		limiter,
+	);
 
-  /**
-   *
-   * ? CLEAN LOCAL REGISTRES (GENERATED FOR dumpCollections)
-   */
-  deleteTempFolder(outputExport);
+	/**
+	 *
+	 * ? CLEAN LOCAL REGISTRES (GENERATED FOR dumpCollections)
+	 */
+	deleteTempFolder(outputExport);
 
-  /**
-   *
-   * ? CLOSE MONGODB CONNECTION
-   */
-  client.close();
+	/**
+	 *
+	 * ? CLOSE MONGODB CONNECTION
+	 */
+	client.close();
 };
 
 export default migrateCollections;
