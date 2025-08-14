a5369814e1ce5fcf06e265c67ee58265f039208d
blpsoares <bryanluccas@hotmail.com>
2025-08-14 17:23:48 -0300

    feat:  implements parallelism

---
+++ src/commands/sync.ts
@@ -0,0 +1,43 @@
+import { cli } from "winston/lib/winston/config";
+import { conn } from "../db/conn";
+import { errorHandler } from "../errors/errorHandler";
+import { eventHandler } from "../functions/documentFunctions";
+import { getCollections } from "../functions/getCollections";
+import type { SyncOptionsCli } from "../types/cliOptions";
+import { syncYmlSchema, type SyncYmlOptions } from "../types/parseYml";
+import parseYml from "../utils/parseYml";
+import Bottleneck from "bottleneck";
+
+export async function syncCollections(
+	ymlPath: string,
+	cliParams: SyncOptionsCli,
+) {
+	const options = parseYml<SyncYmlOptions>(ymlPath, syncYmlSchema);
+	const client = await conn(options.command.sync.source.uri, "source");
+	const db = client.db(options.command.sync.source.db);
+
+	const destClient = await conn(
+		options.command.sync.destination.uri,
+		"destination",
+	);
+
+	const destDb = destClient.db(options.command.sync.destination.db);
+	const limiter = new Bottleneck({ maxConcurrent: cliParams.parallel ?? 3 });
+
+	try {
+		const collections = await getCollections(
+			db,
+			cliParams,
+			ymlPath,
+			options.command.sync.collections,
+		);
+
+		console.log(collections);
+
+		collections.forEach((collectionName) => {
+			limiter.schedule(() => eventHandler(collectionName, db, destDb));
+		});
+	} catch (error) {
+		throw errorHandler(error, "WATCH:COLL");
+	}
+}
