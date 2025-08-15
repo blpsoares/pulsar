0c4865a8a12c35143567b1fb5826e176818b2b60
blpsoares <bryanluccas@hotmail.com>
2025-08-15 15:47:46 -0300

    feat: create watcher do manipulate events

---
+++ src/core/sync/watcherEvents.ts
@@ -0,0 +1,25 @@
+import { errorHandler } from "../../errors/errorHandler";
+import { customLog } from "../../utils/customLog";
+import { dumpCollections } from "./dumpEvent";
+import { watchDeleteEvent } from "./deleteEvent";
+import { watchInsertEvent } from "./insertEvent";
+import { watchUpdateEvent } from "./updateEvent";
+import { EventEmitter } from "node:events";
+
+export const watcher = new EventEmitter();
+
+watcher.on("dump", dumpCollections);
+
+watcher.on("finishDump", (coll: string) =>
+	customLog("success", `Collection [ ${coll} ] dumpada para o destino.`, true),
+);
+watcher.on("errorDump", (err: Error | unknown, coll: string) => {
+	throw errorHandler(err, `DUMP:${coll}`);
+});
+watcher.on("errorDump", (err: Error | unknown, coll: string) => {
+	throw errorHandler(err, `DUMP:${coll}`);
+});
+
+watcher.on("insert", watchInsertEvent);
+watcher.on("update", watchUpdateEvent);
+watcher.on("delete", watchDeleteEvent);
